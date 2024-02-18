4a7b811571add9764826bdfef78b1633ef7f90f8
ahmet-gor-bear <ahmet.gor@bear.us>
2024-02-18 03:42:17 +0300

    info added

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,11 @@ function App() {
     <div className="App">
         <div className="centered-box">
             <div className="box-content">
+
                 <h1>Rick and Morty</h1>
                 <p>multi-select combobox</p>
+                <p>Ahmet GÖR</p>
+                <p><a href={'https://github.com/ahmet-gor-bear/multiselect-rick'}>https://github.com/ahmet-gor-bear/multiselect-rick</a></p>
                 <MultiSelectRick/>
             </div>
         </div>
